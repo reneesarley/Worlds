7c1c55fd8e63cf6385d2c44fac67d2c0d9a0005b
renee sarley and scott fraser <renee@>
2018-07-09 08:41:12 -0700

    add initial file setup

---
+++ World/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace World.Models
+{
+    public class Country
+    {
+        private string _code;
+        private string _name;
+        private string _continent;
+        private string _region;
+        
+        public Country()
+        {
+        }
+    }
+}
---
+++ World/Models/Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using World;
+using MySql.Data.MySqlClient;
+
+
+namespace World.Models
+
+{
+    public class DB
+    {
+        public static MySqlConnection Connection()
+        {
+            MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+            return conn;
+        }
+    }
+}
